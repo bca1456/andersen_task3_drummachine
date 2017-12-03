d187145c8537f66a2819b8afba8de374c85f25ad
bca1456 <eugene.antipenko.s@gmail.com>
2017-12-03 23:37:04 +0300

    added data.json

--- button-listener.js
+++ button-listener.js
@@ -5,7 +5,15 @@ saveButton.addEventListener(eventTypeClick, saveToFile);
 openButton.addEventListener(eventTypeClick, openFromFile);
 
 function openFromFile(){
-
+    var xhr = new XMLHttpRequest(); //ajax запрос
+    xhr.open('GET', 'data.json', false);
+    xhr.send();
+    if (xhr.status != 200) {
+    alert( xhr.status + ': ' + xhr.statusText ); 
+    } else {
+    var data = JSON.parse(xhr.responseText);
+    console.log(data);
+    }
 }
 
 function saveToFile(){
