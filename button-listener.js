5166a0e4ed78f8ccaef462725e72699fb29b6239
bca1456 <eugene.antipenko.s@gmail.com>
2017-12-05 00:11:56 +0300

    added README.md + reconfig package.json

--- button-listener.js
+++ button-listener.js
@@ -14,21 +14,15 @@ function openFromFile(){
     if (xhr.status != 200) {
         alert( xhr.status + ': ' + xhr.statusText ); 
     } else {
-        //simulateClick(200, 200);
-        console.log("all good ");
         var jsonDataArr = JSON.parse(xhr.responseText);
         console.log("length json: " + jsonDataArr.bits.length);
         //debugger;
         for (var i = 0; i < jsonDataArr.bits.length; i++){
             var bit = jsonDataArr.bits[i];
             setupButtonClickingFromFile(bit);
-            //startCoordinateX += BUTTON_SIZE + 13;
-            //startCoordinateY += BUTTON_SIZE + 13;
-            //console.log(startCoordinateX + "  " + startCoordinateY);
         }
         startCoordinateX = 13;
         startCoordinateY = 13;
-        //console.log(jsonDataArr[0]);
     }
 }
 
@@ -40,7 +34,11 @@ function setupButtonClickingFromFile(bit) {
         //debugger;
         var p = { x: startCoordinateX, y: startCoordinateY };
         if (isPointInButton(p, column, row)) {
-          track.steps[column] = !on;
+            if (bit[column] == 1){  //если в файле встретили 1 , то включаем
+                track.steps[column] = true;
+            } else {
+                track.steps[column] = false;
+            }
         }
         startCoordinateX += BUTTON_SIZE + 13;
       });
@@ -50,5 +48,38 @@ function setupButtonClickingFromFile(bit) {
 }
 
 function saveToFile(){
-    
+    const writeJsonFile = require('write-json-file');
+    writeJsonFile('data.json', tracksToJSON()).then(() => {
+        console.log('done');
+    });
+}
+
+function tracksToJSON(){
+    var newJSON = [];
+    var newArr = [];
+        data.tracks.forEach(function(track, row) {
+            
+            track.steps.forEach(function(on, column) {
+            //debugger;
+            var p = { x: startCoordinateX, y: startCoordinateY };
+            if (isPointInButton(p, column, row)) {
+                if (track.steps[column]){  //если в включен звук, то записываем 1 в файл
+                    newArr.push(1);
+                    //jsonDataArr.bits[i][column] = 1;
+                } else {
+                    newArr.push(0);
+                    //jsonDataArr.bits[i][column] = 0;
+                }
+            }
+            startCoordinateX += BUTTON_SIZE + 13;
+            });
+            startCoordinateX = 13;
+            startCoordinateY += BUTTON_SIZE + 13;
+            newJSON.push(newArr);
+            newArr = [];
+        });
+
+    startCoordinateX = 13;
+    startCoordinateY = 13;
+        return newJSON;
 }
